9ab1333ea249d5a13b634edf5bf722810f8b69b9
bfyxzls <bfyxzls@sina.com>
2019-07-01 22:18:25 +0800

    cloud

--- user/src/main/java/com/example/user/config/JPAUserDetailsService.java
+++ user/src/main/java/com/example/user/config/JPAUserDetailsService.java
@@ -1,17 +1,21 @@
 package com.example.user.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JPAUserDetailsService implements UserDetailsService {
+  @Autowired
+  PasswordEncoder passwordEncoder;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    return new User("zzl", "zhanling", AuthorityUtils.NO_AUTHORITIES);
+    return new User("zzl", passwordEncoder.encode("123"), AuthorityUtils.createAuthorityList("user", "admin"));
   }
 }
