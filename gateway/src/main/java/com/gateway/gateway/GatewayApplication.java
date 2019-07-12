7eba96cb121b05a9edba2ef97dfa84c64e51fcf0
zhangzhanling <zhangzl@i-counting.cn>
2019-07-12 16:49:47 +0800

    添加了logback日志

--- gateway/src/main/java/com/gateway/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/gateway/gateway/GatewayApplication.java
@@ -1,6 +1,8 @@
 package com.gateway.gateway;
 
 import com.gateway.gateway.config.TokenContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -14,12 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GatewayApplication {
 
+  private final static Logger logger = LoggerFactory.getLogger(GatewayApplication.class);
+
   public static void main(String[] args) {
     SpringApplication.run(GatewayApplication.class, args);
   }
 
   @GetMapping("/index")
   public String index() {
+    logger.info("hello world!");
     return "hello gateway" + TokenContext.getToken();
   }
 
